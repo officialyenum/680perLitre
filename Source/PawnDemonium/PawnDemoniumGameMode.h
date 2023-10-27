fcb07b58c4000c51e6717be42a6ba27a6a3ef78a
Chukwuyenum Opone <oponechukwuyenum@gmail.com>
2023-10-27 17:38:25 +0100

    Init Commit

---
+++ Source/PawnDemonium/PawnDemoniumGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PawnDemoniumGameMode.generated.h"
+
+UCLASS(minimalapi)
+class APawnDemoniumGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	APawnDemoniumGameMode();
+};
+
+
+
