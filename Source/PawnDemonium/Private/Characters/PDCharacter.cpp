f430e41725c1e1ac8b82b21210c5a25d1ea70993
Chukwuyenum Opone <oponechukwuyenum@gmail.com>
2023-10-28 13:31:43 +0100

    Init Player Movement

--- Source/PawnDemonium/Private/Characters/PDCharacter.cpp
+++ Source/PawnDemonium/Private/Characters/PDCharacter.cpp
@@ -55,21 +55,39 @@ APDCharacter::APDCharacter()
 
 void APDCharacter::Move(const FInputActionValue& Value)
 {
-	// Call the base class  
-	Super::BeginPlay();
+	// input is a Vector2D
+	FVector2D MovementVector = Value.Get<FVector2D>();
 
-	//Add Input Mapping Context
-	if (APlayerController* PlayerController = Cast<APlayerController>(Controller))
+	if (Controller != nullptr)
 	{
-		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
-		{
-			Subsystem->AddMappingContext(DefaultMappingContext, 0);
-		}
+		// find out which way is forward
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+		// get forward vector
+		const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+
+		// get right vector 
+		const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+		// add movement 
+		AddMovementInput(ForwardDirection, MovementVector.Y);
+		AddMovementInput(RightDirection, MovementVector.X);
 	}
+	
 }
 
 void APDCharacter::Look(const FInputActionValue& Value)
 {
+	// input is a Vector2D
+	FVector2D LookAxisVector = Value.Get<FVector2D>();
+
+	if (Controller != nullptr)
+	{
+		// add yaw and pitch input to controller
+		AddControllerYawInput(LookAxisVector.X);
+		AddControllerPitchInput(LookAxisVector.Y);
+	}
 }
 
 void APDCharacter::Shoot(const FInputActionValue& Value)
@@ -100,4 +118,15 @@ void APDCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCompone
 
 void APDCharacter::BeginPlay()
 {
+	// Call the base class  
+	Super::BeginPlay();
+
+	//Add Input Mapping Context
+	if (APlayerController* PlayerController = Cast<APlayerController>(Controller))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(DefaultMappingContext, 0);
+		}
+	}
 }
