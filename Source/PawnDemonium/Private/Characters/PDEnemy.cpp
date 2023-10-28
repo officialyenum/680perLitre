1b7457aa3d80d8d603ed88078e0259bc8ac9d882
Codutola <czawrites@gmail.com>
2023-10-28 23:01:29 +0100

    Started blocking out level.

--- Source/PawnDemonium/Private/Characters/PDEnemy.cpp
+++ Source/PawnDemonium/Private/Characters/PDEnemy.cpp
@@ -3,10 +3,28 @@
 
 #include "Characters/PDEnemy.h"
 
+
 APDEnemy::APDEnemy()
 {
 	// Set Attributes
 	Hp = 100.f;
 	Attack = 10.f;
 	Speed = 100.f;
+}
+
+void APDEnemy::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
+void APDEnemy::BeginPlay()
+{
+	// Call the base class  
+	Super::BeginPlay();
+
+}
+
+void APDEnemy::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
 }
